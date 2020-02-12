fe3ae134fccd36bf3f7d6cea390b3322d9a43ea9
Roy Ge <rgecoder@gmail.com>
2020-02-12 07:10:41 -0800

    refactoring & extracting our routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import DogList from './DogList';
-import DogDetails from './DogDetails';
+import Routes from './Routes';
 import Navbar from './Navbar';
-import {Switch, Route} from 'react-router-dom';
+
 import whiskey from './imgs/whiskey.jpg';
 import hazel from './imgs/hazel.jpg';
 import tubby from './imgs/tubby.jpg';
@@ -46,20 +45,11 @@ class App extends Component {
     ]
   };
   render() {
-    const getDog = props =>{
-      let name = props.match.params.name;
-      let currentDog = this.props.dogs.find(
-        dog => dog.name.toLowerCase() === name.toLowerCase()
-      );
-      return <DogDetails {...props} dog={currentDog} />;
-    };
+    
     return (
       <div className="App">
         <Navbar dogs={this.props.dogs}/>
-        <Switch>
-          <Route exact path='/dogs' render={()=> <DogList dogs={this.props.dogs}/>} />
-          <Route exact path='/dogs/:name' render={getDog} />
-        </Switch>
+        <Routes dogs={this.props.dogs}/>
       </div>
     );
   }
